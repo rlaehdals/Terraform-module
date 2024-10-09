ff99041ea7eab4278e1389b03a181460fe483e70
K dong min <user@MacBook-Pro.local>
2024-10-09 19:17:01 +0900

    Refactor [] 구조 수정

--- lambda/common/main.tf
+++ lambda/common/main.tf
@@ -6,25 +6,21 @@ locals {
 
 resource "aws_lambda_function" "lambda" {
   function_name = var.lambda_name
-
-  image_uri     = var.image_uri
   architectures = var.architectures
-  package_type  = "Image"
-  memory_size   = var.memory_size
 
-  role    = "test"
-  timeout = var.timeout
+  memory_size  = var.memory_size
+  package_type = "Image"
+  image_uri    = var.image_uri
+  timeout      = var.timeout
 
   environment {
     variables = merge(var.variables, local.commons)
-  }
-
-  tracing_config {
-    mode = "Active"
   }
 
   vpc_config {
     security_group_ids = []
     subnet_ids         = []
   }
+
+  role = "test"
 }
